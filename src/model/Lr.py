97aeca75cd54b69b98ac9ba2bf454d561b6204a3
Mohammad Hossein Forouhesh Tehrani <Mohammad.Forouhesh@Outlook.com>
2021-12-26 12:32:35 +0330

    Update Lr.py

---
+++ src/model/Lr.py
@@ -0,0 +1,12 @@
+from src.params import device
+from torch import nn
+import torch.nn
+
+
+class LinearRegression(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(LinearRegression, self).__init__()
+        self.linear = nn.Linear(input_size, output_size)
+
+    def forward(self, x):
+        return self.linear(x)
