0ba373c8950e9aa3032af8a1d9aebd808e8e7b58
Mohammad Hossein Forouhesh Tehrani <Mohammad.Forouhesh@Outlook.com>
2022-01-03 13:37:45 +0330

    Update params.py

--- src/params.py
+++ src/params.py
@@ -1,13 +1,13 @@
 import torch
-
+from sentence_transformers import SentenceTransformer
 
 N_EPOCH = 10
-
+print(torch.__version__)
+emb_model = SentenceTransformer('distilbert-base-multilingual-cased')
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('Using {} device'.format(device))
 
 file = open('persian.txt', 'r', encoding="utf8")
 sw_persian = list(file.read().splitlines())
-
 
 best_validation_loss = float('inf')
