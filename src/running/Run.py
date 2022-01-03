1787db367396b7582f72f2af0de1ccf9276d3104
Mohammad Hossein Forouhesh Tehrani <Mohammad.Forouhesh@Outlook.com>
2022-01-03 15:20:42 +0330

    re-implement cnn
    train acc: 0.992
    test acc:  0.933

--- src/running/Run.py
+++ src/running/Run.py
@@ -16,15 +16,15 @@ def run(model, iterator, optimizer, loss_function, n_epoch, if_lstm=False):
         train_loss, train_acc = train(model, iterator, optimizer, criterion=loss_function, if_lstm=if_lstm)
         
         valid_loss, valid_acc = evaluate(model, iterator, criterion=loss_function)
-        
-        end_time = time.time()
-        epoch_mins, epoch_secs = time_per_epoch(start_time, end_time)
-        
+
         if valid_loss < best_validation_loss:
             best_validation_loss = valid_loss
             torch.save(model.state_dict(), 'models/exa_emotion_classification.pt')
 
         if (epoch + 1) % 10 != 0: continue
+
+        end_time = time.time()
+        epoch_mins, epoch_secs = time_per_epoch(start_time, end_time)
         print(f'Epoch {epoch + 1}, Time: {epoch_mins} mins: {epoch_secs} secs')
         print(f'\t Train Loss {train_loss:.3f}, Train Acc {train_acc * 100:.3f}')
         print(f'\t Valid Loss {valid_loss:.3f}, Valid Acc {valid_acc * 100:.3f}')
