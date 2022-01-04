35d89805d6000553013ca14281e030d4cddc820f
Mohammad Hossein Forouhesh Tehrani <Mohammad.Forouhesh@Outlook.com>
2022-01-04 14:04:03 +0330

    Update Run.py

--- src/running/Run.py
+++ src/running/Run.py
@@ -9,10 +9,10 @@ best_validation_loss = float('inf')
 
 
 def run(model, iterator, optimizer, loss_function, n_epoch, if_lstm=False):
-    
     global best_validation_loss
+    start_time = time.time()
+
     for epoch in range(n_epoch):
-        start_time = time.time()
         train_loss, train_acc = train(model, iterator, optimizer, criterion=loss_function, if_lstm=if_lstm)
         
         valid_loss, valid_acc = evaluate(model, iterator, criterion=loss_function)
@@ -28,5 +28,5 @@ def run(model, iterator, optimizer, loss_function, n_epoch, if_lstm=False):
         print(f'Epoch {epoch + 1}, Time: {epoch_mins} mins: {epoch_secs} secs')
         print(f'\t Train Loss {train_loss:.3f}, Train Acc {train_acc * 100:.3f}')
         print(f'\t Valid Loss {valid_loss:.3f}, Valid Acc {valid_acc * 100:.3f}')
-    
+        start_time = time.time()
     return model
