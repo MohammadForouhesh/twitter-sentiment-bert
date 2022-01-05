0cbb456e18ca3b91e4d9deb2d6f45d1809545b17
Mohammad Hossein Forouhesh Tehrani <Mohammad.Forouhesh@Outlook.com>
2022-01-05 17:09:25 +0330

    Update Cnn.py

--- src/model/Cnn.py
+++ src/model/Cnn.py
@@ -5,8 +5,8 @@ import torch
 
 
 class CNN(nn.Module):
-    def __init__(self, input_size, output_size, n_filters=100, filter_sizes=[1, 1, 1],
-                 dropout=0.5):
+    def __init__(self, input_size, output_size, n_filters=800, filter_sizes=[1, 1, 1, 1, 1],
+                 dropout=0.25):
         super().__init__()
         self.convs = nn.ModuleList([
                                     nn.Conv1d(in_channels=input_size,
