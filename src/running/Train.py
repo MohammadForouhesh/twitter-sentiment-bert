64e66478c38a28741497b17fb76050bce10c2464
Mohammad Hossein Forouhesh Tehrani <Mohammad.Forouhesh@Outlook.com>
2022-01-11 00:56:11 +0330

    biLSTM implemented

--- src/running/Train.py
+++ src/running/Train.py
@@ -3,14 +3,12 @@ from src.params import device
 import torch
 
 
-def train(model, iterator, optimizer, criterion, if_lstm=False):
+def train(model, iterator, optimizer, criterion):
     epoch_loss = 0
     epoch_acc = 0
     model.train()
     for seq, label in iterator:
         optimizer.zero_grad()
-        if if_lstm: model.hidden_cell = (torch.rand(model.lstm.num_layers, seq.shape[0], model.hidden_layer_size).to(device),
-                                         torch.rand(model.lstm.num_layers, seq.shape[0], model.hidden_layer_size).to(device))
         preds = model(seq)
         loss = criterion(preds, label)
         acc = categorical_acc(preds, label)
