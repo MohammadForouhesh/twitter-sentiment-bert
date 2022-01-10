5cac2e92adebc839919ff76fa29efe1c96bbf86c
Mohammad Hossein Forouhesh Tehrani <Mohammad.Forouhesh@Outlook.com>
2022-01-11 00:56:11 +0330

    biLSTM implemented

--- src/model/Lstm.py
+++ src/model/Lstm.py
@@ -29,20 +29,20 @@ class LSTMMemoryGate(nn.Module):
 
 
 class LSTM(nn.Module):
-    def __init__(self, input_size, hidden_layer_size=200, output_size=10, bidirectional=False, n_layers=5, dropout=0.8):
+    def __init__(self, input_size, hidden_layer_size=200, output_size=10, bidirectional=True, n_layers=5, dropout=0.5):
         super().__init__()
         self.hidden_layer_size = hidden_layer_size
-
+        self.num_layers = n_layers * (2 if bidirectional else 1)
         self.lstm = nn.LSTM(input_size, hidden_layer_size, num_layers=n_layers,
                             bidirectional=bidirectional, batch_first=True,
                             dropout=0 if n_layers < 2 else dropout)
-        self.linear = nn.Linear(hidden_layer_size, output_size)
+        self.linear = nn.Linear(hidden_layer_size * 2 if bidirectional else hidden_layer_size, output_size)
 
-        self.hidden_cell = (torch.rand(1, 1, self.hidden_layer_size).to(device),
-                            torch.rand(1, 1, self.hidden_layer_size).to(device))
+        self.hidden_cell = None
 
     def forward(self, input_seq):
-        lstm_out, self.hidden_cell = self.lstm(input_seq.view(len(input_seq), 1, -1))
+        self.hidden_cell = (torch.zeros(self.num_layers, input_seq.size(0), self.hidden_layer_size).to(device),
+                            torch.zeros(self.num_layers, input_seq.size(0), self.hidden_layer_size).to(device))
+        lstm_out, self.hidden_cell = self.lstm(input_seq.view(len(input_seq), 1, -1), self.hidden_cell)
         predictions = self.linear(lstm_out.view(len(input_seq), -1))
-
         return predictions
