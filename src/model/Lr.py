71c82300c50d2773945e81df526b35c96f36ad02
Mohammad Hossein Forouhesh Tehrani <Mohammad.Forouhesh@Outlook.com>
2022-01-16 16:44:04 +0330

    Update Lr.py

--- src/model/Lr.py
+++ src/model/Lr.py
@@ -1,11 +1,21 @@
+import torch.nn.functional as F
 from torch import nn
 import torch.nn
 
 
 class LinearRegression(nn.Module):
-    def __init__(self, input_size, output_size):
-        super(LinearRegression, self).__init__()
-        self.linear = nn.Linear(input_size, output_size)
+    def __init__(self, input_size, output_size, nonlin=F.relu):
+        super().__init__()
+        self.num_units = 100
+        self.nonlin = nonlin
 
-    def forward(self, x):
-        return self.linear(x)
+        self.dense0 = nn.Linear(input_size, 100)
+        self.nonlin = nonlin
+        self.dense1 = nn.Linear(100, 10)
+        self.output = nn.Linear(10, output_size)
+
+    def forward(self, X, **kwargs):
+        X = self.nonlin(self.dense0(X))
+        X = F.relu(self.dense1(X))
+        X = self.output(X)
+        return X
