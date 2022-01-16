4f90f00713a3a657097fc234ff4746404d384b14
Mohammad Hossein Forouhesh Tehrani <Mohammad.Forouhesh@Outlook.com>
2022-01-16 16:43:28 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from src.preprocessing.Preprocessing import correction, preprocess, remove_redundent_characters
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 from torch.utils.data import TensorDataset
 from src.model.Lr import LinearRegression
 from src.params import device, emb_model
 from torch.utils.data import DataLoader
+from skorch import NeuralNetClassifier
 from src.metrics import ir_metrics
 from src.model import LSTM, CNN
 from src.running.Run import run
@@ -19,14 +21,17 @@ import logging
 import torch
 import gc
 
+id2label = {0: 'sad', 1: 'meh', 2: 'happy'}
+label2id = {'sad': 0, 'meh': 1, 'happy': 2}
 
 gc.enable()
 warnings.filterwarnings("ignore", category=SyntaxWarning)
 
 
 def preparation(dataframe, augment=False) -> (np.ndarray, np.ndarray):
-    text = dataframe.text.apply(remove_redundent_characters)
-    label = dataframe.sentiment.apply(lambda item: 1 if item >= 0 else 0)
+    dataframe.fillna('', inplace=True)
+    text = dataframe.text#.apply(remove_redundent_characters)
+    label = dataframe.sentiment.apply(lambda item: label2id[item])
     if augment:
         text1 = text.apply(preprocess)
         text2 = pd.concat([text1.apply(lambda sent: ' '.join(sent.split(' ')[::-1])), text1], ignore_index=True)
@@ -36,18 +41,15 @@ def preparation(dataframe, augment=False) -> (np.ndarray, np.ndarray):
 
         text = pd.concat([raw_text.apply(lambda sent: ' '.join(sent.split(' ')[::-1])), text3], ignore_index=True)
         label = pd.concat([label, label, label, label], ignore_index=True)
-
-    train = text.apply(emb_model.encode)
+    tqdm.pandas()
+    train = text.progress_apply(emb_model.encode, batch_size=128, show_progress_bar=False)
 
     return list(train), list(label)
 
 
-def inference_model(model, sentence):
-    encoded = emb_model.encode([sentence], convert_to_tensor=True)
-    s, h = model(encoded)[0]
-    sigmoid = lambda x: 2/(1+np.e**(-x)) - 1 if x != 0 else 0
-    polarity = sigmoid(h - s)
-    return 'sad' if polarity < -0.2 else 'meh' if -0.2 <= polarity < 0.2 else 'happy'
+def inference(model, sentence):
+    encoded = emb_model.encode([sentence], convert_to_tensor=True, batch_size=128, show_progress_bar=False)
+    return id2label[int(model(encoded)[0].argmax())]
 
 
 def main(args):
@@ -58,13 +60,14 @@ def main(args):
         tqdm.pandas()
         inference_set = pd.read_excel(args.test_path)
         inference_set = inference_set.dropna()
-        inference_set['sentiment'] = inference_set.text.progress_apply(lambda item: inference_model(trained_model, item))
+        inference_set['sentiment'] = inference_set.text.progress_apply(lambda item: inference(trained_model, item))
         inference_set.to_excel('datasets_polarity_lstm.xlsx')
         return 0
 
-    df = pd.read_csv(args.train_path)
+    df = pd.read_excel(args.train_path)
     train_df, test_df = train_test_split(df, test_size=0.15, stratify=list(df.sentiment))
     train_df, eval_df = train_test_split(train_df, test_size=0.1, stratify=list(train_df.sentiment))
+
     X_train, y_train = preparation(train_df, augment=False)
     X_eval, y_eval = preparation(eval_df, augment=False)
     X_test, y_test = preparation(test_df, augment=False)
@@ -86,7 +89,7 @@ def main(args):
     else:                   target = torch.LongTensor(y_test)
     test_ds = TensorDataset(inputs, target)
 
-    batch_size = 16 if args.model_name == 'lstm' else 1
+    batch_size = 32
 
     train_dl = DataLoader(train_ds, batch_size, shuffle=True)
     eval_dl = DataLoader(eval_ds, batch_size, shuffle=True)
@@ -99,25 +102,16 @@ def main(args):
           colored('\n===============TRAIN=' + args.model_name.upper() + '===============', 'red'))
     if args.model_name == 'lstm':
         model = LSTM(input_size=len(X_train[0]), output_size=len(set(y_train))).to(device)
-    
-        optimizer = torch.optim.AdamW(model.parameters(), amsgrad=True)
-    
-        trained_model = run(model=model, train_iterator=train_dl, eval_iterator=eval_dl, optimizer=optimizer,
-                            loss_function=loss_function, n_epoch=args.epoch)
 
     elif args.model_name == 'cnn':
         model = CNN(input_size=len(X_train[0]), output_size=len(set(y_train))).to(device)
-        optimizer = torch.optim.AdamW(model.parameters(), amsgrad=True)
-    
-        trained_model = run(model=model, train_iterator=train_dl, eval_iterator=eval_dl, optimizer=optimizer,
-                            loss_function=loss_function, n_epoch=args.epoch)
 
     elif args.model_name == 'lr':
         model = LinearRegression(input_size=len(X_train[0]), output_size=len(set(y_train))).to(device)
-        optimizer = torch.optim.AdamW(model.parameters(), amsgrad=True)
 
-        trained_model = run(model=model, train_iterator=train_dl, eval_iterator=eval_dl, optimizer=optimizer,
-                            loss_function=loss_function, n_epoch=args.epoch)
+    optimizer = torch.optim.AdamW(model.parameters(), amsgrad=True)
+    trained_model = run(model=model, train_iterator=train_dl, eval_iterator=eval_dl, optimizer=optimizer,
+                        loss_function=loss_function, n_epoch=args.epoch)
 
     ir_metrics(model=trained_model, iterator=train_dl)
 
@@ -125,26 +119,28 @@ def main(args):
           colored('\n===============Test==' + args.model_name.upper() + '===============', 'red'))
 
     ir_metrics(model=trained_model, iterator=test_dl)
+    """
     inference_set = pd.read_csv(args.test_path)
     inference_set['sentiment'] = inference_set.text.apply(lambda item: inference_model(trained_model, item))
     inference_set.to_csv('datasets_sentiment_lstm.csv')
+    """
 
-        
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Neural Architectures')
-    parser.add_argument('--train_path', dest='train_path', type=str, default='dataset/economics_sample-2.csv',
+    parser.add_argument('--train_path', dest='train_path', type=str, default='dataset/sentiment-multilingual-approach.xlsx',
                         help='Raw dataset file address.')
     parser.add_argument('--augment', dest='augment', type=bool, default=True,
                         help='augment the dataset to learn better.')
-    parser.add_argument('--model_name', dest='model_name', type=str, default='lstm',
+    parser.add_argument('--model_name', dest='model_name', type=str, default='cnn',
                         help="supported models in this implementation are CNN and LSTM.")
     parser.add_argument('--preprocess', dest='preprocess', type=bool, default=True,
                         help="whether or not preprocessing the training set.")
-    parser.add_argument('--epoch', dest='epoch', type=int, default=150,
+    parser.add_argument('--epoch', dest='epoch', type=int, default=180,
                         help="number of epochs in the training")
     parser.add_argument('--test_path', dest='test_path', type=str, default='dataset/datasets.xlsx',
                         help="address to test dataset.")
-    parser.add_argument('--load', dest='load', type=str, default='models/sentiment_LSTM.pt')
+    parser.add_argument('--load', dest='load', type=str, default=None)#'models/sentiment_LSTM.pt')
 
     args = parser.parse_args()
 
