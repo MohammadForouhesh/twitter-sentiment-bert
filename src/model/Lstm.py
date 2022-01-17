b4a422fe0901268badb965629acea991267ea061
Mohammad Hossein Forouhesh Tehrani <Mohammad.Forouhesh@Outlook.com>
2022-01-17 20:29:07 +0330

    Update Lstm.py

--- src/model/Lstm.py
+++ src/model/Lstm.py
@@ -29,7 +29,7 @@ class LSTMMemoryGate(nn.Module):
 
 
 class LSTM(nn.Module):
-    def __init__(self, input_size, hidden_layer_size=100, output_size=10, bidirectional=False, n_layers=2, dropout=0.5):
+    def __init__(self, input_size, hidden_layer_size=100, output_size=10, bidirectional=False, n_layers=1, dropout=0.5):
         super().__init__()
         self.hidden_layer_size = hidden_layer_size
         self.num_layers = n_layers * (2 if bidirectional else 1)
