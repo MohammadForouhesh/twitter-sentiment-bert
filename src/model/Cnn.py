88bf2447f44aa24873fdbbc6c34161cd6c9031e9
Mohammad Hossein Forouhesh Tehrani <Mohammad.Forouhesh@Outlook.com>
2022-01-17 14:48:38 +0330

    Update Cnn.py

--- src/model/Cnn.py
+++ src/model/Cnn.py
@@ -5,8 +5,8 @@ import torch
 
 
 class CNN(nn.Module):
-    def __init__(self, input_size, output_size, n_filters=300, filter_sizes=[1, 2, 4, 8, 16],
-                 dropout=0.85):
+    def __init__(self, input_size, output_size, n_filters=50, filter_sizes=(1, 2, 2),
+                 dropout=0.5):
         super().__init__()
         self.convs = nn.ModuleList([
                                     nn.Conv1d(in_channels=input_size,
@@ -14,15 +14,48 @@ class CNN(nn.Module):
                                               kernel_size=fs)
                                     for fs in filter_sizes
                                     ])
-
         self.fc = nn.Linear(len(filter_sizes) * n_filters, output_size)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, text):
-        embedded = text.unsqueeze(0)
-        embedded = embedded.permute(1, 2, 0)
-        conved = [F.relu(conv(embedded)) for conv in self.convs]
-        pooled = [F.max_pool1d(conv, conv.shape[2]).squeeze(2) for conv in conved]
+        embedded = text.unsqueeze(2).expand(-1, -1, 4)
+        conved = [F.leaky_relu(conv(embedded)) for conv in self.convs]
+        pooled = [F.avg_pool1d(conv, conv.shape[2]).squeeze(2) for conv in conved]
+
+        cat = self.dropout(torch.cat(pooled, dim=1))
+        # cat = [batch size, n_filters * len(filter_sizes)]
+        predictions = self.fc(cat.view(len(text), -1))
+        return predictions
+
+
+class CNN2d1(nn.Module):
+    def __init__(self, input_size, output_size, n_filters=300, filter_sizes=(1, 2, 2),
+                 dropout=0.5):
+        super().__init__()
+        self.convs2 = nn.ModuleList([
+                                    nn.Conv2d(in_channels=input_size,
+                                              out_channels=n_filters*2,
+                                              kernel_size=fs)
+                                    for fs in filter_sizes
+                                    ])
+
+        self.convs1 = nn.ModuleList([
+                                    nn.Conv1d(in_channels=n_filters*2,
+                                              out_channels=n_filters,
+                                              kernel_size=fs)
+                                    for fs in filter_sizes
+                                    ])
+        self.fc = nn.Linear(len(filter_sizes) * n_filters, output_size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, text):
+        embedded = text.unsqueeze(2).unsqueeze(3).expand(-1, -1, 32, 32)
+        conved = [F.leaky_relu(conv(embedded)) for conv in self.convs2]
+        pooled2 = [F.avg_pool2d(conv, conv.shape[3]).squeeze(3) for conv in conved]
+
+        cat = self.dropout(torch.cat(pooled2, dim=2))
+        conved = [F.leaky_relu(conv(cat)) for conv in self.convs1]
+        pooled = [F.avg_pool1d(conv, conv.shape[2]).squeeze(2) for conv in conved]
 
         cat = self.dropout(torch.cat(pooled, dim=1))
         # cat = [batch size, n_filters * len(filter_sizes)]
