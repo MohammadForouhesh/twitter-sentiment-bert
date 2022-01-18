d6a772382b67257ae57edc0da94760a7caa4cd56
Mohammad Hossein Forouhesh Tehrani <Mohammad.Forouhesh@Outlook.com>
2022-01-18 14:48:58 +0330

    Create Sentiment.py

---
+++ src/model/Sentiment.py
@@ -0,0 +1,20 @@
+from torch import nn
+import torch
+
+
+class SentimentModel(nn.Module):
+    def __init__(self, embedding, config):
+        super(SentimentModel, self).__init__()
+
+        self.bert = embedding
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+
+    def forward(self, input_ids, attention_mask, token_type_ids):
+        with torch.no_grad():
+            bert_out = self.bert(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+        # last_hidden_state = bert_out.last_hidden_state
+        pooled_output = bert_out.pooler_output
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+        return logits
