c838f138608d136a7d571a1dccbd0167179c02f8
Mohammad Hossein Forouhesh Tehrani <Mohammad.Forouhesh@Outlook.com>
2022-01-18 14:50:01 +0330

    Update Train.py

--- src/running/Train.py
+++ src/running/Train.py
@@ -3,17 +3,26 @@ from src.params import device
 import torch
 
 
-def train(model, iterator, optimizer, criterion):
+def train(model, iterator, optimizer, criterion, scheduler, clip=0.0):
     epoch_loss = 0
     epoch_acc = 0
     model.train()
-    for seq, label in iterator:
+    for dl in iterator:
+        input_ids      = dl['input_ids']
+        attention_mask = dl['attention_mask']
+        token_type_ids = dl['token_type_ids']
+        targets        = dl['targets']
+
         optimizer.zero_grad()
-        preds = model(seq)
-        loss = criterion(preds, label)
-        acc = categorical_acc(preds, label)
+        preds = model(input_ids, attention_mask, token_type_ids)
+        loss = criterion(preds, targets)
+        acc = categorical_acc(preds, targets)
         loss.backward()
+
+        if clip > 0.0:
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=clip)
         optimizer.step()
+        scheduler.step()
         epoch_loss += loss.item()
         epoch_acc += acc.item()
     return epoch_loss / len(iterator), epoch_acc / len(iterator)
